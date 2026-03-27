b48142a891caadd88017b663bbe7ca9cc06419e9
eclipse7-9 <sanzspool@gmail.com>
2026-03-27 09:06:39 -0500

    controller del admin v.2

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,6 +1,54 @@
-﻿namespace MiPrimeraAPI.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MiPrimeraAPI.Data;
+
+namespace MiPrimeraAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize(Roles = "Admin")]
+public class AdminController : ControllerBase
 {
-    public class AdminsController
+    private readonly MediaStoreContext _context;
+    public AdminController(MediaStoreContext context) => _context = context;
+
+    [HttpGet("dashboard")]
+    public async Task<IActionResult> GetDashboard()
     {
+        var totalProductos = await _context.Productos.CountAsync();
+        var totalClientes = await _context.Clientes.CountAsync();
+        var totalVentas = await _context.Ventas.CountAsync();
+        var totalPrestamos = await _context.Prestamos.CountAsync();
+        var ingresoTotal = await _context.Ventas.SumAsync(v => v.Total);
+        var prestamosActivos = await _context.Prestamos
+            .CountAsync(p => p.Estado == 0);
+        var productosConPocoStock = await _context.Inventarios
+            .CountAsync(i => i.StockDisponible <= i.StockMinimo);
+        var ventasRecientes = await _context.Ventas
+            .Include(v => v.Cliente)
+            .OrderByDescending(v => v.Fecha)
+            .Take(5)
+            .Select(v => new {
+                v.Id,
+                v.Fecha,
+                v.Total,
+                v.MetodoPago,
+                v.Estado,
+                Cliente = v.Cliente.Nombre + " " + v.Cliente.Apellido
+            })
+            .ToListAsync();
+
+        return Ok(new
+        {
+            totalProductos,
+            totalClientes,
+            totalVentas,
+            totalPrestamos,
+            ingresoTotal,
+            prestamosActivos,
+            productosConPocoStock,
+            ventasRecientes,
+        });
     }
 }
