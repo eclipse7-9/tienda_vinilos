40d62a3fc29fda774f8c93202a65c5fcc7fd87d0
eclipse7-9 <sanzspool@gmail.com>
2026-04-16 15:15:05 +0000

    Arreglos en el endpoit de logs

--- Data/DbInterceptor.cs
+++ Data/DbInterceptor.cs
@@ -19,16 +19,34 @@ public class PerformanceInterceptor : DbCommandInterceptor
         return base.ReaderExecuted(command, eventData, result);
     }
 
+    public override ValueTask<DbDataReader> ReaderExecutedAsync(DbCommand command, CommandExecutedEventData eventData, DbDataReader result, CancellationToken cancellationToken = default)
+    {
+        LogCommand(command, eventData);
+        return base.ReaderExecutedAsync(command, eventData, result, cancellationToken);
+    }
+
     public override int NonQueryExecuted(DbCommand command, CommandExecutedEventData eventData, int result)
     {
         LogCommand(command, eventData);
         return base.NonQueryExecuted(command, eventData, result);
     }
 
+    public override ValueTask<int> NonQueryExecutedAsync(DbCommand command, CommandExecutedEventData eventData, int result, CancellationToken cancellationToken = default)
+    {
+        LogCommand(command, eventData);
+        return base.NonQueryExecutedAsync(command, eventData, result, cancellationToken);
+    }
+
     public override object? ScalarExecuted(DbCommand command, CommandExecutedEventData eventData, object? result)
     {
         LogCommand(command, eventData);
         return base.ScalarExecuted(command, eventData, result);
+    }
+
+    public override ValueTask<object?> ScalarExecutedAsync(DbCommand command, CommandExecutedEventData eventData, object? result, CancellationToken cancellationToken = default)
+    {
+        LogCommand(command, eventData);
+        return base.ScalarExecutedAsync(command, eventData, result, cancellationToken);
     }
 
     private void LogCommand(DbCommand command, CommandExecutedEventData eventData)
